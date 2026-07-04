276bdf0b188faf9393000af18303b8494e791dea
Christian Serrano <cbi2243012797@izt.uam.mx>
2026-07-04 03:45:10 +0000

    Se agrego la primera accion la cual fue crear la lista desde el constructor, asi como el metodo auxiliar para imprimir los elementos de la lista paso a paso.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,35 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+	//Creando una composicion
+	private ArrayList <String> ListaArray;
+
+	//Constructor, inicializando el objeto ListaArray al crear la App
+	public App(){
+		this.listaArray = new ArrayList<>();
+	}
+
+	public void ejecutarPruebas(){
+	System.out.println("=====Pruebas paso a paso=====\n");
+	}
+
+	System.out.println("--- Creando la lista (desde el constructor) ---");
+	System.out.println("La lista esta vacia? " + this.listaArray.esVacia());
+	imprimirLista();
+
+	private void imprimirLista(){
+	System.out.print("Estado de la lista: [ ");
+		for (int i = 0; i < this.ListaArray.numElementos(); i++){
+		System.out.print(this.ListaArray.consultar(i));
+			if (i < this.ListaArray.numElementos() - 1){
+				System.out.print(" | ");
+			}
+		}
+		System.out.println(" ]");
+	}
+
+	public static void main(String[] args) throws Exception {
+	//En lugar de instanciar el ArrayList, se instancia la App completa, esto para no perder la composicion
+	App miAplicacion = new App();
+	miAplicacion.ejecutarPruebas();
     }
 }
